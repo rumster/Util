8747b99de189903832caf096b689ae6a6762e7c0
rumster <rumster@gmail.com>
2017-06-06 21:59:02 +0300

    Added documentation to BinaryRelation

--- src/bgu/cs/util/BinaryRelation.java
+++ src/bgu/cs/util/BinaryRelation.java
@@ -3,7 +3,7 @@ package bgu.cs.util;
 import java.util.Collection;
 
 /**
- * A set of pairs.
+ * A binary relation is a set of ordered pairs.
  * 
  * @author romanm
  *
@@ -13,23 +13,44 @@ import java.util.Collection;
  *            The type of the second element in every pair of the relation.
  */
 public interface BinaryRelation<E1, E2> {
+	/**
+	 * The number of pairs in the relation.
+	 */
 	public int size();
-	
+
+	/**
+	 * Checks whether there are pairs in the relation.
+	 */
 	public boolean isEmpty();
 
-	public boolean contains(E1 e1, E2 e2);	
+	/**
+	 * Checks whether a given pair of values is a pair in the relation.
+	 * 
+	 * @param e1
+	 *            The first value of the pair.
+	 * @param e2
+	 *            The second value of the pair.
+	 */
+	public boolean contains(E1 e1, E2 e2);
 
+	/**
+	 * Adds a pair to the relation.
+	 * 
+	 * @param e1
+	 *            The first value of the pair.
+	 * @param e2
+	 *            The second value of the pair.
+	 * @return True if the relation changed as the result of this operation.
+	 */
 	public boolean add(E1 e1, E2 e2);
-	
-	//public boolean addAll(Collection<Pair<E1,E2>> pairs);
 
 	public boolean remove(E1 e1, E2 e2);
 
 	public Collection<E2> selectFirst(E1 e1);
 
 	public Collection<E1> selectSecond(E2 e2);
-	
+
 	public Collection<E2> selectFirst();
-	
+
 	public Collection<E1> selectSecond();
 }
